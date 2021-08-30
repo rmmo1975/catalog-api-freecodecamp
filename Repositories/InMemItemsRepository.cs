cbf8273a5573b87df95eb1b53bfcce1fdbdba61d
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-08-30 14:20:23 -0300

    added basic inMem repository;

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepositories
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Portion",
+                Price = 9,
+                CreatedAt = DateTimeOffset.UtcNow
+            },
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Iron Sword",
+                Price = 20,
+                CreatedAt = DateTimeOffset.UtcNow
+            },
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Bronze Shield",
+                Price = 18,
+                CreatedAt = DateTimeOffset.UtcNow
+            },   
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}

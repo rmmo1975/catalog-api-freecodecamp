29f482c39bfa0b8ca21acf34cb12f52f56473c60
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-08-30 14:41:38 -0300

    Added Items Controller;

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+
+            return items;
+        }
+
+    }
+}

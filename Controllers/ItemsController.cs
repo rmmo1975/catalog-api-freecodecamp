7c6f3bb062a9d1a76855966b6bd959922e849a4a
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-08-31 00:15:12 -0300

    added UpdateItem method in the interface and implemented it in the concrete class; created dto to update item; added method in the controller to update an item;

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -51,5 +51,25 @@ namespace Catalog.Controllers
 
             return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item itemToUpdate = existingItem with {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            
+            repository.UpdateItem(itemToUpdate);
+
+            return NoContent();
+        }
     }
 }
--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -45,5 +45,11 @@ namespace Catalog.Repositories
         {
             items.Add(item);
         }
+
+        public void UpdateItem(Item item)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+            items[index] = item;
+        }
     }
 }

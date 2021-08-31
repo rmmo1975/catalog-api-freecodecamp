6a8d817e2325916c08fba0b96a2f05071712b772
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-08-30 23:17:12 -0300

    created AsDto extension method and applied in the controller methods Item object is returned;

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Catalog.Dtos;
 using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -19,21 +20,21 @@ namespace Catalog.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-            var items = repository.GetItems();
+            var items = repository.GetItems().Select(item => item.AsDto());
             return items;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id) {
+        public ActionResult<ItemDto> GetItem(Guid id) {
             var item = repository.GetItem(id);
 
             if(item is null)
             {
                 return NotFound();
             }
-            return item;
+            return item.AsDto();
         }
     }
 }

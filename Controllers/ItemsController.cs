e43b4a9d5a96a06cc28cc85c96c2bfaa755f0692
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-08-30 22:51:18 -0300

    Merged in branch feature/02-adding-entity-repository-controller
    commit e79a03ebc48a839dbdc5f5745d8cce247fb20ef9
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 22:48:58 2021 -0300
    
        added return NotFound when no item is found in method GetItem;
    
    commit b91199cb035957ecdbe7fc26243c2a1907e8a2dd
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 22:44:46 2021 -0300
    
        added the GetItem method to return an item by its id;
    
    commit 431a5afd7b2c03072f5ab42ee06a25e01b538142
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 14:41:59 2021 -0300
    
        fixed a small typo in InMemRepository;
    
    commit 29f482c39bfa0b8ca21acf34cb12f52f56473c60
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 14:41:38 2021 -0300
    
        Added Items Controller;
    
    commit cbf8273a5573b87df95eb1b53bfcce1fdbdba61d
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 14:20:23 2021 -0300
    
        added basic inMem repository;
    
    commit 3ea5df20b0e6901b5e8d166865ab35faa601b2f6
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 14:12:33 2021 -0300
    
        item entity created;
    
    commit 427f61ab51ef8773dbcb333bcf3c3e52e8cd7ba4
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Mon Aug 30 14:09:49 2021 -0300
    
        removed all the unnecessary files to initiate the projec;
    
    Changes to be committed:
    	new file:   Controllers/ItemsController.cs
    	deleted:    Controllers/WeatherForecastController.cs
    	new file:   Entities/Item.cs
    	new file:   Repositories/InMemItemsRepository.cs
    	deleted:    WeatherForecast.cs

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id) {
+            var item = repository.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}

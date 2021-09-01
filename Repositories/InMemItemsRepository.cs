78b1bfcc823d43f154eeeb48958c19c9dd64cb38
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-09-01 14:21:50 -0300

    modified "in memory items repository" to be used asynchronously; added "completedTask" to add asychronous behaviour to this repository;

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Catalog.Entities;
 
 namespace Catalog.Repositories
@@ -32,30 +33,34 @@ namespace Catalog.Repositories
             },
         };
 
-        public IEnumerable<Item> GetItems()
+        public async Task<IEnumerable<Item>> GetItemsAsync()
         {
-            return items;
-        }
-        public Item GetItem(Guid id)
-        {
-            return items.Where(item => item.Id == id).SingleOrDefault();
+            return await Task.FromResult(items);
         }
 
-        public void CreateItem(Item item)
+        public async Task<Item> GetItemAsync(Guid id)
+        {
+            return await Task.FromResult(items.Where(item => item.Id == id).SingleOrDefault());
+        }
+
+        public async Task CreateItemAsync(Item item)
         {
             items.Add(item);
+            await Task.CompletedTask;
         }
 
-        public void UpdateItem(Item item)
+        public async Task UpdateItemAsync(Item item)
         {
-            var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+            var index = items.FindIndex(item => item.Id == item.Id);
             items[index] = item;
+            await Task.CompletedTask;
         }
 
-        public void DeleteItem(Guid id)
+        public async Task DeleteItemAsync(Guid id)
         {
-            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            var index = items.FindIndex(item => item.Id == id);
             items.RemoveAt(index);
+            await Task.CompletedTask;
         }
     }
 }

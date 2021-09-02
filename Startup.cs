ba18d6619fa1902d8860cc94bfda34e9591fef6e
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-09-02 00:50:51 -0300

    Merged in branch feature/07-secrets-and-health-checks
    commit 25f6c5480ec6fd5364e36682736496512de04871
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Thu Sep 2 00:49:27 2021 -0300
    
        added custom health check response;
    
    commit de89be9a125cc3891cef873620d8eba41001e65f
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Thu Sep 2 00:35:36 2021 -0300
    
        added checks for readness and liveness;
    
    commit d0cdc5f2e090e5909d3f98ebcffdf09d9c130879
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Thu Sep 2 00:29:54 2021 -0300
    
        added mongodb health check;
    
    commit 58dde89a45a2da70a0995531a2c56047b94ce594
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Wed Sep 1 22:54:00 2021 -0300
    
        added endpoint for health checks;
    
    commit 3ae986b6d691fb256fdf20c7f8bf6d81cb52ea24
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Wed Sep 1 22:46:27 2021 -0300
    
        added mongodb credentials in the service;
    
    commit 65dbcaf20193ad7bf2f29ed0dbfb3ce36cbb0706
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Wed Sep 1 22:41:18 2021 -0300
    
        added the property User in MongoDbSettings;
        added .net secret manager;
    
    commit 4fe0e41bb3cb0b23c8311d1006e1105aab663417
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Wed Sep 1 22:32:58 2021 -0300
    
        added authentication on our instance of mongodb to be used locally;

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -34,11 +38,11 @@ namespace Catalog
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
             
             services.AddSingleton<IMongoClient>(serviceProvider => 
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
             
             services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
@@ -53,6 +57,13 @@ namespace Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "catalog_api_freecodecamp", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                .AddMongoDb(mongoDbSettings.ConnectionString,
+                    name: "mongodb",
+                    timeout: TimeSpan.FromSeconds(3),
+                    tags: new [] { "ready" }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +85,30 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapHealthChecks("/health-check/ready", new HealthCheckOptions{
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async(context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new{
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+
+                endpoints.MapHealthChecks("/health-check/live", new HealthCheckOptions{
+                    Predicate = (_) => false
+                });
             });
         }
     }

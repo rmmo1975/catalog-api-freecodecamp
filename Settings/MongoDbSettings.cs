3ae986b6d691fb256fdf20c7f8bf6d81cb52ea24
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-09-01 22:46:27 -0300

    added mongodb credentials in the service;

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -4,10 +4,11 @@
     {
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
         public string ConnectionString
         {
-            get { return $"mongodb://{Host}:{Port}"; }
+            get { return $"mongodb://{User}:{Password}@{Host}:{Port}"; }
         }
-        
     }
 }
